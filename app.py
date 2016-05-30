bcc113fcd9a0b6ffb229928bc66057779ab0ed9a
Evan Hildebrandt <evan.hildebrandt@redbrickmedia.com>
2016-05-30 14:08:02 -0700

    add db

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
+from flask.ext.mysql import MySQL
 app = Flask(__name__)
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'ilikepie111'
+app.config['MYSQL_DATABASE_DB'] = 'saiddit'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
 
 @app.route("/")
 def main():
-	return render_template('index.html')
+	cursor.execute("show databases")
+	tables = cursor.fetchall()
+	return render_template('index.html', tables=tables)
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
